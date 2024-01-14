a592779bb595b67ad45182cb3f40824b5f502b04
SBSun <46879319+SBSun@users.noreply.github.com>
2024-01-14 22:13:40 +0900

    Feat: 프로젝트 기본 세팅

---
+++ src/main/java/connectingstar/tars/TarsApplication.java
@@ -0,0 +1,13 @@
+package connectingstar.tars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/connectingstar/tars/TarsApplicationTests.java
@@ -0,0 +1,13 @@
+package connectingstar.tars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
