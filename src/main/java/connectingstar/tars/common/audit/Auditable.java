0ef724be559dec95fca87ae230029262ce011944
HeWillGoTillTheEnd <kingofunder@gmail.com>
2024-02-01 21:36:45 +0900

    TS-202 Feat: 생성시각, 수정 시각 기록용 Auditable 추가

---
+++ src/main/java/connectingstar/tars/common/audit/Auditable.java
@@ -0,0 +1,28 @@
+package connectingstar.tars.common.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+    @CreatedDate
+    @Column(name = "created_at",updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
+
+
