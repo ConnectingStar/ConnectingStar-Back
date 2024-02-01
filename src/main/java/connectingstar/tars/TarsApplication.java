d70a4702af73a0b78e59f220e17ec56b5b5a6f76
HeWillGoTillTheEnd <kingofunder@gmail.com>
2024-02-01 21:36:52 +0900

    TS-202 Feat: 생성시각, 수정 시각 기록용 Auditable 추가

--- src/main/java/connectingstar/tars/TarsApplication.java
+++ src/main/java/connectingstar/tars/TarsApplication.java
@@ -2,8 +2,10 @@ package connectingstar.tars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TarsApplication {
 
 	public static void main(String[] args) {
