85999a03e7416888f16928f0c05cd276bf7da8f9
HeWillGoTillTheEnd <kingofunder@gmail.com>
2024-02-18 18:50:36 +0900

    TS-205 Feat: 월간 습관기록 조회

--- src/main/java/connectingstar/tars/habit/repository/QuitHabitDao.java
+++ src/main/java/connectingstar/tars/habit/repository/QuitHabitDao.java
@@ -3,19 +3,13 @@ package connectingstar.tars.habit.repository;
 import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import connectingstar.tars.constellation.domain.QConstellation;
-import connectingstar.tars.constellation.response.ConstellationListResponse;
 import connectingstar.tars.habit.domain.QQuitHabit;
 import connectingstar.tars.habit.request.QuitHabitListRequest;
 import connectingstar.tars.habit.response.QuitHabitListResponse;
-import connectingstar.tars.user.domain.QUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Objects;
-
-import static connectingstar.tars.user.domain.QUser.user;
 
 /**
  * 종료한 습관 Repository
@@ -38,12 +32,12 @@ public class QuitHabitDao {
 
     public List<QuitHabitListResponse> getList(QuitHabitListRequest param) {
         QQuitHabit quitHabit = QQuitHabit.quitHabit;
-        // 타입별 회원이 보유중인 별자리 조회
-            return queryFactory
-                    .select(getConstructorExpression())
-                    .from(quitHabit)
-                    .where(quitHabit.user.Id.eq(param.getUserId()))
-                    .fetch();
+
+        return queryFactory
+                .select(getConstructorExpression())
+                .from(quitHabit)
+                .where(quitHabit.user.Id.eq(param.getUserId()))
+                .fetch();
     }
 
     private ConstructorExpression<QuitHabitListResponse> getConstructorExpression() {
