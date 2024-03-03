77984c6ed631615d9901bf2a73da34b35fd127e2
bnfkim <kgr3819@gmail.com>
2024-03-03 22:06:18 +0900

    TS-207 Feat: OAuth2User

--- src/main/java/connectingstar/tars/user/ouath/OAuth2UserInfo.java
+++ src/main/java/connectingstar/tars/user/ouath/OAuth2UserInfo.java
@@ -9,7 +9,7 @@ public abstract class OAuth2UserInfo {
         this.attributes = attributes;
     }
 
-    public abstract String getId(); //소셜 식별 값 : 구글 - "sub", 카카오 - "id", 네이버 - "id"
+    public abstract String getId();
 
     public abstract String getNickname();
 
