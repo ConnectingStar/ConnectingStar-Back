f279b42ee2f0c8d9a6131811cb77ec7838fa0c0d
bnfkim <kgr3819@gmail.com>
2024-03-03 22:07:42 +0900

    TS-207 Feat: KakaoOAuth2UserInfo

--- src/main/java/connectingstar/tars/user/kakao/KakaoOAuth2UserInfo.java
+++ src/main/java/connectingstar/tars/user/kakao/KakaoOAuth2UserInfo.java
@@ -1,2 +1,52 @@
-package connectingstar.tars.user.kakao;public class KakaoOAuth2UserInfo {
+package connectingstar.tars.user.kakao;
+
+import connectingstar.tars.user.ouath.OAuth2UserInfo;
+
+import java.util.Map;
+
+public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
+
+    public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return String.valueOf(attributes.get("id"));
+    }
+
+    @Override
+    public String getNickname() {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+
+        if (account == null) {
+            return null;
+        }
+
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+
+        if (profile == null) {
+            return null;
+        }
+
+        return (String) profile.get("nickname");
+    }
+
+    @Override
+    public String getImageUrl() {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+
+        if (account == null) {
+            return null;
+        }
+
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+
+        if (profile == null) {
+            return null;
+        }
+
+        return (String) profile.get("thumbnail_image_url");
+    }
 }
+
