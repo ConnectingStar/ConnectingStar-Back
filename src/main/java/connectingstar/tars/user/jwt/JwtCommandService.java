3751afddf8963a877b54a2548b5cefb8b2dd3ab9
bnfkim <kgr3819@gmail.com>
2024-03-07 10:16:51 +0900

    TS-207 Feat: JwtCommandService - SendRefreshToken & AccessToken

--- src/main/java/connectingstar/tars/user/jwt/JwtCommandService.java
+++ src/main/java/connectingstar/tars/user/jwt/JwtCommandService.java
@@ -80,6 +80,13 @@ public class JwtCommandService {
         log.info("재발급된 Access Token : {}", accessToken);
     }
 
+    public void sendRefreshToken(HttpServletResponse response, String refreshToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setHeader(refreshHeader, refreshToken);
+
+        log.info("Access Token, Refresh Token");
+    }
+
     /**
      * 헤더에서 RefreshToken 추출
      * 토큰 형식 : Bearer XXX에서 Bearer를 제외하고 순수 토큰만 가져오기 위해서
@@ -87,6 +94,17 @@ public class JwtCommandService {
      */
     public Optional<String> extractRefreshToken(HttpServletRequest request) {
         return Optional.ofNullable(request.getHeader(refreshHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+
+    /**
+     * 헤더에서 AccessToken 추출
+     * 토큰 형식 : Bearer XXX에서 Bearer를 제외하고 순수 토큰만 가져오기 위해서
+     * 헤더를 가져온 후 "Bearer"를 삭제(""로 replace)
+     */
+    public Optional<String> extractAccessToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(accessHeader))
                 .filter(refreshToken -> refreshToken.startsWith(BEARER))
                 .map(refreshToken -> refreshToken.replace(BEARER, ""));
     }
