9ae109292dbe001873c12f2e033b96430f17aa98
bnfkim <kgr3819@gmail.com>
2024-03-10 19:33:06 +0900

    TS-212 Feat : RunHabitRepository findAllByUser 추가

--- src/main/java/connectingstar/tars/habit/repository/RunHabitRepository.java
+++ src/main/java/connectingstar/tars/habit/repository/RunHabitRepository.java
@@ -1,7 +1,12 @@
 package connectingstar.tars.habit.repository;
 
 import connectingstar.tars.habit.domain.RunHabit;
+import connectingstar.tars.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RunHabitRepository extends JpaRepository<RunHabit,Integer> {
+
+    List<RunHabit> findAllByUser(User user);
 }
