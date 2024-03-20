864adfc1b19a320e1f38cc7a4e1715d320a6845b
bnfkim <kgr3819@gmail.com>
2024-03-20 10:59:42 +0900

    TS-214 Feat: ConstellationHving Entity + Repository 삭제

--- src/main/java/connectingstar/tars/constellation/repository/ConstellationHavingRepository.java
+++
@@ -1,7 +0,0 @@
-package connectingstar.tars.constellation.repository;
-
-import connectingstar.tars.constellation.domain.ConstellationHaving;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConstellationHavingRepository extends JpaRepository<ConstellationHaving, Integer> {
-}
