35d43732e4c44a4baddce01828deb100413f59c2
bnfkim <kgr3819@gmail.com>
2024-03-20 09:44:09 +0900

    TS-214 Feat: ConstellationHving Entity + Repository 생성

---
+++ src/main/java/connectingstar/tars/constellation/repository/ConstellationHavingRepository.java
@@ -0,0 +1,7 @@
+package connectingstar.tars.constellation.repository;
+
+import connectingstar.tars.constellation.domain.ConstellationHaving;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConstellationHavingRepository extends JpaRepository<ConstellationHaving, Integer> {
+}
