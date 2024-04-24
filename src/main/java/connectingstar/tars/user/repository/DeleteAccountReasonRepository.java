55df1c1c5180e24b28b79156624b53529c915fde
Gyuri Kim <kgr3819@gmail.com>
2024-04-24 10:38:41 +0900

    TS-318 : AccountReason_탈퇴이유생성 API CRUD  (#62)

--- src/main/java/connectingstar/tars/user/repository/DeleteAccountReasonRepository.java
+++ src/main/java/connectingstar/tars/user/repository/DeleteAccountReasonRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserOutRepository extends JpaRepository<DeleteAccountReason, Integer> {
+public interface DeleteAccountReasonRepository extends JpaRepository<DeleteAccountReason, Integer> {
 
 }
