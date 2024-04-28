6a7a8a8d90a646a0aa6fd104f18283834b0e554f
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2024-04-28 15:23:42 +0900

    feat: cors 설정 추가 (#64)

---
+++ src/main/java/connectingstar/tars/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package connectingstar.tars.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS 설정
+ *
+ * @author 송병선
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+        .allowedHeaders("*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+        .allowCredentials(true);
+  }
+}
