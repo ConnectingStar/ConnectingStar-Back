c9e19d19b00eddb59596a26ac5ce8dc33108c9de
ByungSun Song <byung0216@gmail.com>
2024-05-09 22:12:45 +0900

    fix: 모든 API 공통 응답 형식 적용

--- src/main/java/connectingstar/tars/notice/controller/AlertStopController.java
+++ src/main/java/connectingstar/tars/notice/controller/AlertStopController.java
@@ -1,32 +1,37 @@
 package connectingstar.tars.notice.controller;
+
+import connectingstar.tars.common.response.SuccessResponse;
 import connectingstar.tars.notice.command.AlertStopCommandService;
 import connectingstar.tars.notice.request.AlertStopDeleteRequest;
 import connectingstar.tars.notice.request.AlertStopRequest;
 import connectingstar.tars.notice.validation.AlertStopValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping(value = "/alert", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AlertStopController {
 
-    private final AlertStopCommandService alertStopCommandService;
-    @PostMapping
-    public ResponseEntity<?> createAlertStop(@RequestBody AlertStopRequest alertStopRequest) {
-        alertStopCommandService.saveAlertStop(alertStopRequest);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+  private final AlertStopCommandService alertStopCommandService;
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteAlertStop(@RequestBody AlertStopDeleteRequest param) {
-        AlertStopValidator.validate(param);
-        alertStopCommandService.removeAlertStop(param);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+  @PostMapping
+  public ResponseEntity<?> createAlertStop(@RequestBody AlertStopRequest alertStopRequest) {
+    alertStopCommandService.saveAlertStop(alertStopRequest);
+    return ResponseEntity.ok(new SuccessResponse());
+  }
+
+  @DeleteMapping
+  public ResponseEntity<?> deleteAlertStop(@RequestBody AlertStopDeleteRequest param) {
+    AlertStopValidator.validate(param);
+    alertStopCommandService.removeAlertStop(param);
+    return ResponseEntity.ok(new SuccessResponse());
+  }
 
 }
