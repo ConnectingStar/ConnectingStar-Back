203da151cf1b6287dddd1ab422fb424cfa4c4ab7
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2024-05-15 18:59:45 +0900

    Revert "fix: logout 문제 해결확인 테스트" (#83)
    Reverts ConnectingStar/ConnectingStar-Back#82
    
    문제가 있는것으로 확인되어 롤백합니다.

--- src/main/java/connectingstar/tars/common/config/SecurityConfig.java
+++ src/main/java/connectingstar/tars/common/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import connectingstar.tars.auth.filter.JwtAuthenticationFilter;
 import connectingstar.tars.auth.filter.JwtExceptionFilter;
 import connectingstar.tars.common.handler.CustomAccessDeniedHandler;
 import connectingstar.tars.common.handler.CustomAuthenticationEntryPoint;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +13,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Security Configuration
@@ -60,18 +56,5 @@ public class SecurityConfig {
         .securityContext((securityContext) -> securityContext.requireExplicitSave(false));
 
     return http.build();
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOriginPatterns(List.of("*"));
-    configuration.addAllowedMethod("*");
-    configuration.addAllowedHeader("*");
-    configuration.setExposedHeaders(List.of("*"));
-    configuration.setAllowCredentials(true);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
   }
 }
