52958a5fc91b43e76778d67e19f95278ddd684ae
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2024-05-28 12:39:57 +0900

    Feat/onboard exception (#93)

--- src/main/java/connectingstar/tars/common/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/connectingstar/tars/common/handler/CustomAuthenticationEntryPoint.java
@@ -28,29 +28,12 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException authException) {
-    if(request.getRequestURI().equals("/user/check-onboarding")) {
-      setFalseResponse(response);
-      return;
-    }
-
     ErrorResponse errorResponse = new ErrorResponse(UNAUTHORIZED_USER);
     response.setContentType("application/json;charset=UTF-8");
     response.setStatus(errorResponse.status());
-
+    
     try {
       response.getWriter().print(new ObjectMapper().writeValueAsString(errorResponse));
-    } catch (Exception e) {
-      log.error(e.getMessage());
-    }
-  }
-
-  private void setFalseResponse(HttpServletResponse response) {
-    DataResponse dataResponse = new DataResponse(new UserOnboardCheckResponse(false));
-    response.setContentType("application/json;charset=UTF-8");
-    response.setStatus(HttpStatus.OK.value());
-
-    try {
-      response.getWriter().print(new ObjectMapper().writeValueAsString(dataResponse));
     } catch (Exception e) {
       log.error(e.getMessage());
     }
