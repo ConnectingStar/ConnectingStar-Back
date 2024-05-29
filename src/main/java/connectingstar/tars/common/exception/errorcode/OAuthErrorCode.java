a87142c7cd4be837a1cda6c8bd6ef847f2d90a29
ByungSun Song <byung0216@gmail.com>
2024-05-30 00:34:45 +0900

    fix: 카카오 로그인 api 예외 핸들링 설정

--- src/main/java/connectingstar/tars/common/exception/errorcode/OAuthErrorCode.java
+++ src/main/java/connectingstar/tars/common/exception/errorcode/OAuthErrorCode.java
@@ -19,7 +19,10 @@ public enum OAuthErrorCode implements ErrorCode {
   PARAM_SOCIAL_TYPE_EMPTY(HttpStatus.BAD_REQUEST, "소셜 타입은 필수값입니다."),
   PARAM_AUTH_CODE_EMPTY(HttpStatus.BAD_REQUEST, "Auth Code는 필수값입니다."),
 
-  SOCIAL_TYPE_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 소셜 타입입니다.");
+  SOCIAL_TYPE_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 소셜 타입입니다."),
+
+  OAUTH_TOKEN_FAIL(HttpStatus.BAD_REQUEST, "oauth token 발급에 실패했습니다."),
+  OAUTH_USER_FAIL(HttpStatus.BAD_REQUEST, "oauth 사용자 정보 조회에 실패했습니다.");
 
   private final HttpStatus httpStatus;
   private final String message;
