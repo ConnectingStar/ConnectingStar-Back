7005e576c7893bbb80268bb01969b4f9ed6efc08
Gyuri Kim <kgr2626@naver.com>
2024-06-07 12:26:11 +0900

    Fix : token 으로 다시 변수명 변경

--- src/main/java/connectingstar/tars/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/connectingstar/tars/auth/filter/JwtAuthenticationFilter.java
@@ -1,24 +1,21 @@
 package connectingstar.tars.auth.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import connectingstar.tars.common.response.DataResponse;
-import connectingstar.tars.common.response.ErrorResponse;
-import connectingstar.tars.user.response.UserOnboardCheckResponse;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
 import connectingstar.tars.auth.JwtService;
 import connectingstar.tars.common.config.JwtProperties;
+import connectingstar.tars.common.response.DataResponse;
 import connectingstar.tars.common.utils.CookieUtils;
+import connectingstar.tars.user.response.UserOnboardCheckResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
  * JwtAuthenticationFilter
@@ -32,13 +29,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   private final JwtService jwtService;
   private final JwtProperties jwtProperties;
 
+  private static void writeFalseValue(HttpServletResponse response) throws IOException {
+    DataResponse dataResponse = new DataResponse(new UserOnboardCheckResponse(false));
+    response.setContentType("application/json;charset=UTF-8");
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.getWriter().print(new ObjectMapper().writeValueAsString(dataResponse));
+  }
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    String accessToken = CookieUtils.getCookie(request, jwtProperties.cookieName());
+    String token = CookieUtils.getCookie(request, jwtProperties.cookieName());
 
-    if (!StringUtils.hasText(accessToken)) {
-      if(request.getRequestURI().equals("/user/check-onboarding")) {
+    if (!StringUtils.hasText(token)) {
+      if (request.getRequestURI().equals("/user/check-onboarding")) {
         writeFalseValue(response);
         return;
       }
@@ -47,17 +51,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     // 유효성 체크
-    if (jwtService.validateToken(accessToken)) {
-      SecurityContextHolder.getContext().setAuthentication(jwtService.getAuthentication(accessToken));
+    if (jwtService.validateToken(token)) {
+      SecurityContextHolder.getContext().setAuthentication(jwtService.getAuthentication(token));
     }
 
     filterChain.doFilter(request, response);
-  }
-
-  private static void writeFalseValue(HttpServletResponse response) throws IOException {
-    DataResponse dataResponse = new DataResponse(new UserOnboardCheckResponse(false));
-    response.setContentType("application/json;charset=UTF-8");
-    response.setStatus(HttpServletResponse.SC_OK);
-    response.getWriter().print(new ObjectMapper().writeValueAsString(dataResponse));
   }
 }
