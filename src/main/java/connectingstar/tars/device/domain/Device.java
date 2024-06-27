6b9cbd251e3486cb0c6af00318bbbbdeb8df2c62
Woojin Lee <dev.woojin.lee@gmail.com>
2024-06-27 09:53:57 +0900

    feat: (device) 도메인 정의

---
+++ src/main/java/connectingstar/tars/device/domain/Device.java
@@ -0,0 +1,49 @@
+package connectingstar.tars.device.domain;
+
+import connectingstar.tars.common.audit.Auditable;
+import connectingstar.tars.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+/**
+ * 유저가 보유한 기기 (스마트폰)
+ * 알림을 보낼 때 사용한다
+ *
+ * @author 이우진
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Access(AccessType.FIELD)
+public class Device extends Auditable {
+    @Id
+    @Column(name = "device_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 디바이스를 소유하고 있는 회원
+     */
+    @OneToOne
+    @JoinColumn(name = "owning_user_id")
+    private User owningUser;
+
+    /**
+     * FCM Registration Token.
+     * Firebase로 알림을 보낼 때 사용하는 기기의 토큰.
+     * 기기 하나 당 하나의 토큰이 발급된다
+     */
+    @Column(name = "fcm_registration_token")
+    private String fcmRegistrationToken;
+
+    /**
+     * FCM Registration Token의 활성화 여부
+     * 만료된 토큰이면 false
+     */
+    @Column(name = "is_fcm_token_active")
+    @ColumnDefault("1")
+    private Boolean isFcmTokenActive;
+}
