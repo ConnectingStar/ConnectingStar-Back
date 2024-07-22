195f66aca00ba970499510e82009889f88ce407c
Woojin Lee <dev.woojin.lee@gmail.com>
2024-07-22 15:00:03 +0900

    feature: (habit-history-date-get) add response dto mapper

--- src/main/java/connectingstar/tars/habit/mapper/HabitHistoryMapper.java
+++ src/main/java/connectingstar/tars/habit/mapper/HabitHistoryMapper.java
@@ -1,20 +1,26 @@
 package connectingstar.tars.habit.mapper;
 
 import connectingstar.tars.habit.domain.HabitHistory;
+import connectingstar.tars.habit.domain.RunHabit;
 import connectingstar.tars.habit.dto.HabitHistoryDto;
+import connectingstar.tars.habit.response.HabitHistoryDateGetResponse;
 import connectingstar.tars.habit.response.HabitHistoryPostResponse;
 import connectingstar.tars.habit.response.HabitHistoryRestPostResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {RunHabitMapper.class})
 public interface HabitHistoryMapper {
     HabitHistoryMapper INSTANCE = Mappers.getMapper(HabitHistoryMapper.class);
 
     @Mapping(source = "user.id", target = "userId")
     @Mapping(source = "runHabit.runHabitId", target = "runHabitId")
     HabitHistoryDto toDto(HabitHistory habitHistory);
+
+    @Mapping(source = "habitHistory", target = "habitHistory")
+    @Mapping(source = "runHabit", target = "runHabit")
+    HabitHistoryDateGetResponse toDateGetResponse(HabitHistory habitHistory, RunHabit runHabit);
 
     @Mapping(source = "habitHistory", target = "habitHistory")
     HabitHistoryPostResponse toPostResponse(HabitHistory habitHistory);
