9e45ef205ed8f344c5f8275671a3363e1b913bfe
Woojin Lee <dev.woojin.lee@gmail.com>
2024-07-24 16:20:22 +0900

    feature: (user-me) add response dto and mapper

--- src/main/java/connectingstar/tars/user/mapper/UserMapper.java
+++ src/main/java/connectingstar/tars/user/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import connectingstar.tars.constellation.mapper.ConstellationMapper;
 import connectingstar.tars.user.domain.User;
 import connectingstar.tars.user.dto.UserDto;
 import connectingstar.tars.user.dto.UserWithConstellationDto;
+import connectingstar.tars.user.response.UserMeGetResponse;
 import connectingstar.tars.user.response.UserMeProfileGetResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -21,6 +22,9 @@ public interface UserMapper {
     @Mapping(source = "user.identity", target = "identity")
     @Mapping(source = "constellation", target = "constellation")
     UserWithConstellationDto toWithConstellationDto(User user, Constellation constellation);
+
+    @Mapping(source = "user", target = "user")
+    UserMeGetResponse toMeGetResponse(User user);
 
     @Mapping(target = "user", expression = "java(toWithConstellationDto(user, constellation))")
     UserMeProfileGetResponse toMeProfileGetResponse(User user, Constellation constellation);
