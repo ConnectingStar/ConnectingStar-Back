7c95618d61889aa0abc2e96ff7a15ac49966c1c3
Woojin Lee <dev.woojin.lee@gmail.com>
2024-07-27 02:17:17 +0900

    docs: (user-constellation) add deprecated comments

--- src/main/java/connectingstar/tars/user/request/UserConstellationRequest.java
+++ src/main/java/connectingstar/tars/user/request/UserConstellationRequest.java
@@ -7,13 +7,15 @@ import lombok.Setter;
  * 별자리 선택(해금 시작), 프로필 별자리 수정 요청 공통 형식
  *
  * @author 송병선
+ * @deprecated use {@link UserMeConstellationPatchRequest} instead
  */
+@Deprecated
 @Getter
 @Setter
 public class UserConstellationRequest {
 
-  /**
-   * 별자리 ID
-   */
-  private Integer constellationId;
+    /**
+     * 별자리 ID
+     */
+    private Integer constellationId;
 }
