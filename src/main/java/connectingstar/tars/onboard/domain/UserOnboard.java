4e267ca5686f2573f62eb73d663921b53643f2ab
Woojin Lee <dev.woojin.lee@gmail.com>
2024-08-02 15:06:30 +0900

    feature: (user-onboard) add upate is user updated

--- src/main/java/connectingstar/tars/onboard/domain/UserOnboard.java
+++ src/main/java/connectingstar/tars/onboard/domain/UserOnboard.java
@@ -2,6 +2,8 @@ package connectingstar.tars.onboard.domain;
 
 import connectingstar.tars.user.domain.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
@@ -11,10 +13,12 @@ import org.hibernate.annotations.ColumnDefault;
  * <p>
  * 온보딩 페이지에서 유저 업데이트, 습관 생성을 모두 완료해야 온보딩이 완료처리 됩니다.
  * 온보딩 완료 정보는 user.onboard 필드에 저장됩니다. (캐싱 용도)
+ *
+ * @author 이우진
  */
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserOnboard {
     /**
      * 온보딩 정보의 고유 id
@@ -44,4 +48,17 @@ public class UserOnboard {
     @Column(name = "is_habit_created", nullable = false)
     @ColumnDefault("0")
     private Boolean isHabitCreated = false;
+
+    public void updateIsUserUpdated(Boolean isUserUpdated) {
+        this.isUserUpdated = isUserUpdated;
+    }
+
+    public void updateIsHabitCreated(Boolean isHabitCreated) {
+        this.isHabitCreated = isHabitCreated;
+    }
+
+    @Builder
+    public UserOnboard(User user) {
+        this.user = user;
+    }
 }
