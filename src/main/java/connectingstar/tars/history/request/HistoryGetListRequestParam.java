4d40dc0ed4c4f6f3a9c56c52533a583ca22b1099
Woojin Lee <dev.woojin.lee@gmail.com>
2024-08-19 11:00:43 +0900

    feature: (history) add get list request param dto

--- src/main/java/connectingstar/tars/history/request/HistoryGetListRequestParam.java
+++ src/main/java/connectingstar/tars/history/request/HistoryGetListRequestParam.java
@@ -1,10 +1,24 @@
 package connectingstar.tars.history.request;
 
+import connectingstar.tars.common.request.param.PaginationRequestParam;
+import connectingstar.tars.common.request.param.RelatedRequestParam;
+import connectingstar.tars.common.request.param.SortRequestParam;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class HistoryGetListRequestParam {
-    g
+public class HistoryGetListRequestParam implements PaginationRequestParam, SortRequestParam, RelatedRequestParam {
+    // Pagination
+    private Integer page;
+
+    private Integer size;
+
+    // Sort
+    private String sortBy;
+
+    private String order;
+
+    // Related
+    private String related;
 }
