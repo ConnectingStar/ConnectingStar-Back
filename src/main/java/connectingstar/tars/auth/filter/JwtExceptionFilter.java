6511547de17fb6485a770597bb68fc034bb02e59
Woojin Lee <dev.woojin.lee@gmail.com>
2024-08-26 21:07:23 +0900

    fix: (jwt) error code type cast error

--- src/main/java/connectingstar/tars/auth/filter/JwtExceptionFilter.java
+++ src/main/java/connectingstar/tars/auth/filter/JwtExceptionFilter.java
@@ -1,15 +1,9 @@
 package connectingstar.tars.auth.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
 import connectingstar.tars.common.config.JwtProperties;
 import connectingstar.tars.common.exception.ValidationException;
-import connectingstar.tars.common.exception.errorcode.AuthErrorCode;
+import connectingstar.tars.common.exception.errorcode.ErrorCode;
 import connectingstar.tars.common.response.ErrorResponse;
 import connectingstar.tars.common.utils.CookieUtils;
 import jakarta.servlet.FilterChain;
@@ -18,6 +12,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 /**
  * JwtExceptionFilter
@@ -29,31 +27,31 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
 
-  private final JwtProperties jwtProperties;
+    private final JwtProperties jwtProperties;
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws ServletException, IOException {
-    try {
-      filterChain.doFilter(request, response);
-    } catch (ValidationException e) {
-      // 쿠키 삭제
-      CookieUtils.setCookie(jwtProperties.cookieName(), null, 0, response);
-      AuthErrorCode authErrorCode = (AuthErrorCode) e.getErrorCode();
-      setErrorResponse(response, authErrorCode);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (ValidationException e) {
+            // 쿠키 삭제
+            CookieUtils.setCookie(jwtProperties.cookieName(), null, 0, response);
+            ErrorCode errorCode = (ErrorCode) e.getErrorCode();
+            setErrorResponse(response, errorCode);
+        }
     }
-  }
 
-  private void setErrorResponse(HttpServletResponse response, AuthErrorCode authErrorCode) {
-    ErrorResponse errorResponse = new ErrorResponse(authErrorCode.getHttpStatus().value(), authErrorCode.getMessage());
+    private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode) {
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getHttpStatus().value(), errorCode.getMessage());
 
-    response.setContentType("application/json;charset=UTF-8");
-    response.setStatus(authErrorCode.getHttpStatus().value());
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(errorCode.getHttpStatus().value());
 
-    try {
-      response.getWriter().print(new ObjectMapper().writeValueAsString(errorResponse));
-    } catch (Exception e) {
-      log.error(e.getMessage());
+        try {
+            response.getWriter().print(new ObjectMapper().writeValueAsString(errorResponse));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
-  }
 }
