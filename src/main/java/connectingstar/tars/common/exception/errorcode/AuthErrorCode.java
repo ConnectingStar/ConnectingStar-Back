35e4d1f938daf539b3ddfafdd6b891901ea8d87c
muddie <parkjm0817@gmail.com>
2024-10-10 18:59:57 +0900

    fix: 엑세스토큰 NULL 에러코드 401->400으로 변경

--- src/main/java/connectingstar/tars/common/exception/errorcode/AuthErrorCode.java
+++ src/main/java/connectingstar/tars/common/exception/errorcode/AuthErrorCode.java
@@ -17,7 +17,7 @@ public enum AuthErrorCode implements ErrorCode {
   INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 서명입니다."),
   EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다."),
   UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "지원하지 않는 JWT 토큰입니다."),
-  NULL_TOKEN(HttpStatus.UNAUTHORIZED, "토큰 값이 NULL입니다."),
+  NULL_TOKEN(HttpStatus.BAD_REQUEST, "토큰 값이 NULL입니다."),
   ILLEGAL_ARGUMENT_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 JWT 토큰입니다."),
 
 
